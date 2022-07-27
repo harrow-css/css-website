bf9bd6658cc1eedeb7df36b801adcf07df5fbb64
Dylan K <kainth.dylan@gmail.com>
2022-07-27 11:52:49 +0100

    installed express

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,10 @@ export default {
     '@nuxtjs/axios',
   ],
 
+  serverMiddleware: [
+    { path: "/api", handler: "~/api/index.js" },
+  ],
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
