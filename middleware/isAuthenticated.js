52befac83c3de511246f069b970e4b46a67ac608
Dylan K <50585244+dylankainth@users.noreply.github.com>
2022-10-08 15:11:45 +0100

    Update isAuthenticated.js

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -6,7 +6,7 @@ export default function({ store, redirect }) {
     const email = (store.$auth.$storage.getUniversal('jwt_decoded').upn)
     const domain = email.split('@').pop()
     
-    if (domain=='harrowschool.org.uk' || email='chun-pei_chan@oxcoll.com') {
+    if (domain=='harrowschool.org.uk' || email=='chun-pei_chan@oxcoll.com') {
     } else {
       return redirect("/auth/errors/notharrowschool");
     }
