2742258a01a3ed27193b8bc40a15ecdfaee36d7f
dylankainth <kainth.dylan@gmail.com>
2024-04-29 20:38:23 +0100

    how does this not work ?!

--- api/src/functions/getUser.js
+++ api/src/functions/getUser.js
@@ -1,33 +1,14 @@
 const { app } = require('@azure/functions');
-const MongoClient = require("mongodb").MongoClient;
-
-const MONGODB_URI = process.env.MONGODB_URI;
-const DB_NAME = 'test';
 
 app.http('getUser', {
-  methods: ['GET', 'POST'],
-  authLevel: 'anonymous',
-  handler: async (event, context) => {
-    // otherwise the connection will never complete, since
-    // we keep the DB connection alive
-    context.callbackWaitsForEmptyEventLoop = false;
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
 
-    const client = await MongoClient.connect(uri, {
-      useUnifiedTopology: true,
-    });
-  
-    db = await client.db(DB_NAME);
+        const name = request.query.get('name') || await request.text() || 'world';
 
-    // query the database for hackathons
-    const user = await db.collection("users").find({_id: event.queryStringParameters.id}).toArray();
-
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    };
-  }
+        return { body: `Hello, ${name}!` };
+    }
 });
 
