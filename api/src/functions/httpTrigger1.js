af3fc72f669b185006ad32c43f836536c24c060e
dylankainth <kainth.dylan@gmail.com>
2024-04-29 22:09:13 +0100

    mongodb print secret

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -6,6 +6,10 @@ app.http('httpTrigger1', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
+        // print secret
+        context.log(`Secret value: ${process.env.MONGODB_URI}`);
+        console.log(process.env.MONGODB_URI);
+
         const name = request.query.get('name') || await request.text() || 'world';
 
         return { body: `Hello, ${name}!` };
