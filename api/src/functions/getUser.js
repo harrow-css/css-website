3cddd3e2ba739f16a4468274cd99f7f385f88fb2
dylankainth <kainth.dylan@gmail.com>
2024-04-29 20:46:21 +0100

    chatgpt answer?

--- api/src/functions/getUser.js
+++ api/src/functions/getUser.js
@@ -1,14 +1,64 @@
 const { app } = require('@azure/functions');
+const { MongoClient } = require("mongodb");
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = 'test';
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db(DB_NAME);
+
+  return cachedDb;
+};
+
+const queryDatabase = async (db, id) => {
+  // query the database for hackathons
+  const user = await db.collection("users").find({ _id: id }).toArray();
+
+  return {
+    status: 200,
+    body: user,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+};
 
 app.http('getUser', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+  methods: ['GET'],
+  authLevel: 'anonymous',
+  handler: async (context, req) => {
+    // otherwise the connection will never complete, since
+    // we keep the DB connection alive
+    context.res = {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+      },
+    };
 
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
+    if (req.method === "OPTIONS") {
+      context.res.status = 200;
+      return;
     }
-});
 
+    const db = await connectToDatabase(MONGODB_URI);
+    const id = req.query.id;
+    const result = await queryDatabase(db, id);
+
+    context.res = {
+      ...context.res,
+      ...result,
+    };
+  }
+});
