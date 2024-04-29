e1fecb2efae94409c5729917e35bbcdd031ab69a
dylankainth <kainth.dylan@gmail.com>
2024-04-29 10:50:51 +0100

    added a second http trigger function

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -11,3 +11,16 @@ app.http('httpTrigger1', {
         return { body: `Hello, ${name}!` };
     }
 });
+
+app.http('httpTrigger2', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}!` };
+    }
+});
+
